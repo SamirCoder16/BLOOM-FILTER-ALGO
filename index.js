74386b4c398e9c52bccee0ed407112bfacbc9282
SamirCoder16 <samirparvez48@gmail.com>
2026-03-05 19:42:27 +0530

    update spacing

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const {BloomFilter} = require('./bloom_filter');
+const { BloomFilter } = require("./bloom_filter");
 
 const bloom = new BloomFilter(1000, 3);
-
 
 bloom.add("samir");
 bloom.add("kaushik");
 bloom.add("twiter");
-
 
 console.log(bloom.alreadyExist("samir")); // TRUE
 console.log(bloom.alreadyExist("twiter")); // TRUE
